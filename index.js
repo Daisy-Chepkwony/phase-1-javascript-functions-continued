c09f67d1bb9867fb8aa597b0a304d8149ab6f3c1
+ <+>
2022-11-25 19:58:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(fun='roller-skate'){
+    console.log(`This Saturday, I want to ${fun}!`);
+
+}
+saturdayFun();
+saturdayFun('This Saturday, I want to bathe my dog!')
+
+
+const mondayWork = function(work='go to the office'){
+    console.log(`${work}`);
+
+}
+mondayWork();
+mondayWork('This Monday, I will work from home.');
+
+
+function wrapAdjective(){
+
+    return function(wrap="*"){
+        console.log(`You were ${wrap}`)
+
+    }    
+}
